e38dba000f2ffd7524eff1830a8053c7c1d3c988
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-03-13 17:08:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-variable "space_count" {
+variable "total_spaces" {
   default = 45
 }
 
 resource "spacelift_space" "spaces" {
-  count            = var.space_count
+  count            = var.total_spaces
   name             = "space-${count.index + 1}"
-  description      = "This is space number ${count.index + 1}."
-  parent_space_id  = count.index == 0 ? "root" : spacelift_space.spaces[count.index - 1].id
+  description      = "This is space ${count.index + 1}."
   inherit_entities = true
 
-  # Ensure Terraform creates spaces in order
+  parent_space_id = count.index == 0 ? "root" : spacelift_space.spaces[count.index - 1].id
+
   depends_on = count.index == 0 ? [] : [spacelift_space.spaces[count.index - 1]]
 }
