7d5d114d8769ae2ab8de0ca9d57b58786fa109f8
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-03-13 17:01:07 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-locals {
-  space_count = 45
-  spaces = { for i in range(1, local.space_count + 1) :
-    format("space-%d", i) => {
-      name        = format("space-%d", i)
-      parent_id   = i == 1 ? "root" : spacelift_space[format("space-%d", i - 1)].id
-      description = "This is space number ${i}."
-    }
-  }
+variable "space_count" {
+  default = 45
 }
 
 resource "spacelift_space" "spaces" {
-  for_each         = local.spaces
+  for_each = { for i in range(1, var.space_count + 1) :
+    format("space-%d", i) => {
+      name        = format("space-%d", i)
+      parent_id   = i == 1 ? "root" : spacelift_space.spaces[format("space-%d", i - 1)].id
+      description = "This is space number ${i}."
+    }
+  }
+
   name             = each.value.name
   description      = each.value.description
   parent_space_id  = each.value.parent_id
