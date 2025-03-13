d2f9257a041299fcddbd954769a4e8be8504af1a
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-03-13 17:02:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,9 @@ variable "space_count" {
 }
 
 resource "spacelift_space" "spaces" {
-  for_each = { for i in range(1, var.space_count + 1) :
-    format("space-%d", i) => {
-      name        = format("space-%d", i)
-      parent_id   = i == 1 ? "root" : spacelift_space.spaces[format("space-%d", i - 1)].id
-      description = "This is space number ${i}."
-    }
-  }
-
-  name             = each.value.name
-  description      = each.value.description
-  parent_space_id  = each.value.parent_id
+  count            = var.space_count
+  name             = "space-${count.index + 1}"
+  description      = "This is space number ${count.index + 1}."
+  parent_space_id  = count.index == 0 ? "root" : element(spacelift_space.spaces[*].id, count.index - 1)
   inherit_entities = true
 }
