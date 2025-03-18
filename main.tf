3a3b4aa41e5fd8724d4dc756297ac6ee7ef4d044
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-03-18 15:49:57 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,158 +1,61 @@
 resource "spacelift_space" "first" {
-  description      = "This is a child of the root space."
   inherit_entities = true
   name             = "first"
   parent_space_id  = "root"
 }
 
-resource "spacelift_space" "second" {
-  description      = "This is a child of the first space."
+resource "spacelift_space" "first_security" {
   depends_on       = [spacelift_space.first]
   inherit_entities = true
-  name             = "second"
+  name             = "first_security"
   parent_space_id  = spacelift_space.first.id
 }
 
-resource "spacelift_space" "third" {
-  description      = "This is a child of the second space."
-  depends_on       = [spacelift_space.second]
+resource "spacelift_space" "first_security_audit" {
+  depends_on       = [spacelift_space.first_security]
   inherit_entities = true
-  name             = "third"
-  parent_space_id  = spacelift_space.second.id
+  name             = "first-security-audit"
+  parent_space_id  = spacelift_space.first_security.id
 }
 
-resource "spacelift_space" "fourth" {
-  description      = "This is a child of the third space."
-  depends_on       = [spacelift_space.third]
+resource "spacelift_space" "first_security_logs" {
+  depends_on       = [spacelift_space.first_security]
   inherit_entities = true
-  name             = "fourth"
-  parent_space_id  = spacelift_space.third.id
+  name             = "first-security-logs"
+  parent_space_id  = spacelift_space.first_security.id
 }
 
-resource "spacelift_space" "fifth" {
-  description      = "This is a child of the fourth space."
-  depends_on       = [spacelift_space.fourth]
+resource "spacelift_space" "first_shared" {
+  depends_on       = [spacelift_space.first]
   inherit_entities = true
-  name             = "fifth"
-  parent_space_id  = spacelift_space.fourth.id
+  name             = "shared"
+  parent_space_id  = spacelift_space.first.id
 }
 
-resource "spacelift_space" "sixth" {
-  description      = "This is a child of the fifth space."
-  depends_on       = [spacelift_space.fifth]
+resource "spacelift_space" "first_shared_iam" {
+  depends_on       = [spacelift_space.first_shared]
   inherit_entities = true
-  name             = "sixth"
-  parent_space_id  = spacelift_space.fifth.id
+  name             = "first-shared-iam"
+  parent_space_id  = spacelift_space.first_shared.id
 }
 
-resource "spacelift_space" "seventh" {
-  description      = "This is a child of the sixth space."
-  depends_on       = [spacelift_space.sixth]
+resource "spacelift_space" "first_shared_monitoring" {
+  depends_on       = [spacelift_space.first_shared]
   inherit_entities = true
-  name             = "seventh"
-  parent_space_id  = spacelift_space.sixth.id
+  name             = "first-shared-monitoring"
+  parent_space_id  = spacelift_space.first_shared.id
 }
 
-resource "spacelift_space" "eighth" {
-  description      = "This is a child of the seventh space."
-  depends_on       = [spacelift_space.seventh]
+resource "spacelift_space" "first_shared_resources" {
+  depends_on       = [spacelift_space.first_shared]
   inherit_entities = true
-  name             = "eighth"
-  parent_space_id  = spacelift_space.seventh.id
+  name             = "first-shared-resources"
+  parent_space_id  = spacelift_space.first_shared.id
 }
 
-resource "spacelift_space" "ninth" {
-  description      = "This is a child of the eighth space."
-  depends_on       = [spacelift_space.eighth]
+resource "spacelift_space" "first_shared_tgw" {
+  depends_on       = [spacelift_space.first_shared]
   inherit_entities = true
-  name             = "ninth"
-  parent_space_id  = spacelift_space.eighth.id
-}
-
-resource "spacelift_space" "tenth" {
-  description      = "This is a child of the ninth space."
-  depends_on       = [spacelift_space.ninth]
-  inherit_entities = true
-  name             = "tenth"
-  parent_space_id  = spacelift_space.ninth.id
-}
-
-resource "spacelift_space" "eleventh" {
-  description      = "This is a child of the tenth space."
-  depends_on       = [spacelift_space.tenth]
-  inherit_entities = true
-  name             = "eleventh"
-  parent_space_id  = spacelift_space.tenth.id
-}
-
-resource "spacelift_space" "twelfth" {
-  description      = "This is a child of the eleventh space."
-  depends_on       = [spacelift_space.eleventh]
-  inherit_entities = true
-  name             = "twelfth"
-  parent_space_id  = spacelift_space.eleventh.id
-}
-
-resource "spacelift_space" "thirteenth" {
-  description      = "This is a child of the twelfth space."
-  depends_on       = [spacelift_space.twelfth]
-  inherit_entities = true
-  name             = "thirteenth"
-  parent_space_id  = spacelift_space.twelfth.id
-}
-
-resource "spacelift_space" "fourteenth" {
-  description      = "This is a child of the thirteenth space."
-  depends_on       = [spacelift_space.thirteenth]
-  inherit_entities = true
-  name             = "fourteenth"
-  parent_space_id  = spacelift_space.thirteenth.id
-}
-
-resource "spacelift_space" "fifteenth" {
-  description      = "This is a child of the fourteenth space."
-  depends_on       = [spacelift_space.fourteenth]
-  inherit_entities = true
-  name             = "fifteenth"
-  parent_space_id  = spacelift_space.fourteenth.id
-}
-
-resource "spacelift_space" "sixteenth" {
-  description      = "This is a child of the fifteenth space."
-  depends_on       = [spacelift_space.fifteenth]
-  inherit_entities = true
-  name             = "sixteenth"
-  parent_space_id  = spacelift_space.fifteenth.id
-}
-
-resource "spacelift_space" "seventeenth" {
-  description      = "This is a child of the sixteenth space."
-  depends_on       = [spacelift_space.sixteenth]
-  inherit_entities = true
-  name             = "seventeenth"
-  parent_space_id  = spacelift_space.sixteenth.id
-}
-
-resource "spacelift_space" "eighteenth" {
-  description      = "This is a child of the seventeenth space."
-  depends_on       = [spacelift_space.seventeenth]
-  inherit_entities = true
-  name             = "eighteenth"
-  parent_space_id  = spacelift_space.seventeenth.id
-}
-
-resource "spacelift_space" "nineteenth" {
-  description      = "This is a child of the eighteenth space."
-  depends_on       = [spacelift_space.eighteenth]
-  inherit_entities = true
-  name             = "nineteenth"
-  parent_space_id  = spacelift_space.eighteenth.id
-}
-
-resource "spacelift_space" "twentieth" {
-  description      = "This is a child of the nineteenth space."
-  depends_on       = [spacelift_space.nineteenth]
-  inherit_entities = true
-  name             = "twentieth"
-  parent_space_id  = spacelift_space.nineteenth.id
+  name             = "first-shared-tgw"
+  parent_space_id  = spacelift_space.first_shared.id
 }
